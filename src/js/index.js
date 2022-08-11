2163abfbd588a6acab2709d93af6c681babfec32
Thalles <engenharia3@mestria.com.br>
2022-08-11 12:41:03 -0300

    Implementou acesso a Pokemon API, troca foto e id.

--- src/js/index.js
+++ src/js/index.js
@@ -5,23 +5,53 @@ listaCartaoPokemons.forEach(pokemon => {
   pokemon.addEventListener("click", () => {
     //Removendo cartão do pokemon aberto.
     const cartaoPokemonAberto = document.querySelector(".aberto");
-    cartaoPokemonAberto.classList.remove("aberto");
+    if (cartaoPokemonAberto) {
+      cartaoPokemonAberto.classList?.remove("aberto");
+    }
 
     //Removendo cartão ativo marcado na lista de pokémon.
     const pokemonAtivoNaListagem = document.querySelector(".ativo");
-    pokemonAtivoNaListagem.classList.remove("ativo");
+    if (pokemonAtivoNaListagem) {
+      pokemonAtivoNaListagem.classList?.remove("ativo");
+    }
 
     //Ao clicar em um pokémon da listagem pegamos o id desse pokémon pra saber qual cartão mostrar.
     const idPokemonSelecionado = pokemon.attributes.id.value;
-    const idDoCartaoPokemonParaAbrir = `${idPokemonSelecionado}-card`;
+    const idDoCartaoPokemonParaAbrir = `cartao-${idPokemonSelecionado}`;
+
+    getPokemon(idPokemonSelecionado, idDoCartaoPokemonParaAbrir);
+
     const cartaoPokemonParaAbrir = document.getElementById(
       idDoCartaoPokemonParaAbrir
     );
-    cartaoPokemonParaAbrir.classList.add("aberto");
+    cartaoPokemonParaAbrir.classList?.add("aberto");
 
     //Deixar selecionado o cartão na lista de pokemon escolido.
     const pokemonSelecionadoNaListagem =
       document.getElementById(idPokemonSelecionado);
-    pokemonSelecionadoNaListagem.classList.add("ativo");
+    pokemonSelecionadoNaListagem.classList?.add("ativo");
   });
 });
+
+function getPokemon(_pokemon, card) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${_pokemon}`;
+
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const id = `${data.id}`.padStart(3, 0);
+      document.querySelector(`#${card} .detalhes span`).innerText = `#${id}`;
+
+      let img = data.sprites.other.home.front_default;
+      document
+        .querySelector(`#${card}  .cartao-imagem img`)
+        .setAttribute("src", img);
+
+      console.log(data.name);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
